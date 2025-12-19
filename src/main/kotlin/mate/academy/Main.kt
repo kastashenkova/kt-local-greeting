d02723b2cc3e6dbe581afc799dc8b64f4813a285
kastashenkova <astashenkova.katya@gmail.com>
2025-12-20 01:37:45 +0200

    fixed fun placement

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,16 +1,15 @@
 package mate.academy
 
-fun greetUsers() {
-    while (true) {
-        println("Please enter a name (or press Enter to quit):")
-        val input = readLine() ?: return
-        if (input.isEmpty()) {
-            return
-        }
-        println("Hello, $input!")
-    }
-}
-
 fun main() {
+    fun greetUsers() {
+        while (true) {
+            println("Please enter a name (or press Enter to quit):")
+            val input = readLine() ?: return
+            if (input.isEmpty()) {
+                return
+            }
+            println("Hello, $input!")
+        }
+    }
     greetUsers()
 }
