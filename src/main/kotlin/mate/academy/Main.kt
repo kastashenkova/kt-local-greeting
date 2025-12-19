02d3600af189903d45f09bcd14dcbaedf435dd38
kastashenkova <astashenkova.katya@gmail.com>
2025-12-20 01:30:17 +0200

    create local fun

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,11 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    while (true) {
+        print("Please enter a name (or press Enter to quit):")
+        val input = readLine() ?: return
+        print("Hello, $input!")
+    }
 }
 
 fun main() {
